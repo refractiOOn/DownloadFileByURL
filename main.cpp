0c43d46c7d2d6eb3c4bd87c63f3f6d53ce1ab33f
refractiOOn <sotnikovroman.work@gmail.com>
2024-06-12 07:23:43 -0700

    implement run function

--- main.cpp
+++ main.cpp
@@ -10,11 +10,40 @@ bool download(std::wstring_view url, std::wstring_view dest)
     return SUCCEEDED(res);
 }
 
+bool run(std::wstring_view path, std::wstring_view params)
+{
+    std::wstring command = std::wstring(path) + L" " + std::wstring(params);
+    STARTUPINFOW startupInfo = { sizeof(startupInfo )};
+    PROCESS_INFORMATION procInfo;
+
+    if (!CreateProcessW(nullptr, command.data(), nullptr, nullptr, FALSE, 0, nullptr, nullptr, &startupInfo, &procInfo))
+    {
+        return false;
+    }
+
+    WaitForSingleObject(procInfo.hProcess, INFINITE);
+    CloseHandle(procInfo.hProcess);
+    CloseHandle(procInfo.hThread);
+
+    return true;
+}
+
 int wmain(int argc, wchar_t **argv)
 {
     const std::wstring url = L"https://zoom.us/client/latest/ZoomInstaller.exe";
     const std::wstring destination = L"C:/Users/Refraction/Downloads/zoom_installer.exe";
 
-    if (!download(url, destination)) std::cout << "File was not loaded" << std::endl;
-    else std::cout << "File was successfully downloaded" << std::endl;
+    if (!download(url, destination))
+    {
+        std::wcout << "File was not loaded" << std::endl;
+        return -1;
+    }
+    else std::wcout << "File was successfully downloaded" << std::endl;
+
+    if (!run(destination, L"-h"))
+    {
+        std::wcout << "Could not start specified executable" << std::endl;
+    }
+
+    return 0;
 }
