2c7d8ca91c50b3d60543e5c18bcc1cca5e63b6d6
refractiOOn <sotnikovroman.work@gmail.com>
2024-06-12 08:25:33 -0700

    implement cmd argument parsing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <vector>
 #include <Windows.h>
 
 // Link required library
 #pragma comment(lib, "urlmon.lib")
+
+std::vector<std::wstring> parseArgs(const int argc, wchar_t **argv)
+{
+    std::vector<std::wstring> args;
+    for (int i = 1; i < argc; ++i)
+    {
+        args.emplace_back(argv[i]);
+    }
+    return args;
+}
 
 bool download(std::wstring_view url, std::wstring_view dest)
 {
@@ -10,10 +21,11 @@ bool download(std::wstring_view url, std::wstring_view dest)
     return SUCCEEDED(res);
 }
 
-bool run(std::wstring_view path, std::wstring_view params)
+bool run(std::wstring_view path, std::wstring_view params = std::wstring_view())
 {
-    std::wstring command = std::wstring(path) + L" " + std::wstring(params);
-    STARTUPINFOW startupInfo = { sizeof(startupInfo )};
+    std::wstring command = params.empty() ? std::wstring(path)
+                                          : std::wstring(path) + L" " + std::wstring(params);
+    STARTUPINFOW startupInfo = { sizeof(startupInfo) };
     PROCESS_INFORMATION procInfo;
 
     if (!CreateProcessW(nullptr, command.data(), nullptr, nullptr, FALSE, 0, nullptr, nullptr, &startupInfo, &procInfo))
@@ -30,8 +42,15 @@ bool run(std::wstring_view path, std::wstring_view params)
 
 int wmain(int argc, wchar_t **argv)
 {
-    const std::wstring url = L"https://zoom.us/client/latest/ZoomInstaller.exe";
-    const std::wstring destination = L"C:/Users/Refraction/Downloads/zoom_installer.exe";
+    const std::vector<std::wstring> args = parseArgs(argc, argv);
+
+    if (args.size() < 2)
+    {
+        std::wcerr << "No URL and destination arguments were provided" << std::endl;
+        return -1;
+    }
+    const std::wstring url = args[0];
+    const std::wstring destination = args[1];
 
     if (!download(url, destination))
     {
@@ -40,9 +59,11 @@ int wmain(int argc, wchar_t **argv)
     }
     else std::wcout << "File was successfully downloaded" << std::endl;
 
-    if (!run(destination, L"-h"))
+    const std::wstring params = args.size() == 3 ? args[2] : std::wstring();
+    if (!run(destination, params))
     {
         std::wcout << "Could not start specified executable" << std::endl;
+        return -1;
     }
 
     return 0;
